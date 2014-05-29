df910fa83d0b9f8a627e752b5b16966dee9dd45e
Henryvw <Henryvw@gmail.com>
2014-05-29 09:33:56 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StoicCompass::Application.config.secret_key_base = 'f55fe4c5fed917de07c2783bfcb32651a8e3f34e877e0400c8f499d6cf4b7b409a8a4524b5330e51f61887ac2003aef48eb9d2d9da1ebb985dea1d444cdff44a'
