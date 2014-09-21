db09cd241949a0ca6769a8d31b75a821d54e4833
Henryvw <Henryvw@gmail.com>
2014-09-21 11:30:06 +0200

    installed cocoon. add question works. remove question still not working despite correctly adding to strong params

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,6 +1,6 @@
 class Exercise < ActiveRecord::Base
   has_many :e_answers, through: :e_questions
-  has_many :e_questions
+  has_many :e_questions, :dependent => :destroy
   has_many :rehearsals
   accepts_nested_attributes_for :rehearsals
   accepts_nested_attributes_for :e_questions, :reject_if => lambda { |a| a[:question].blank? }, allow_destroy: true
