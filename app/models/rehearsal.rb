47bb24fdc49f575a21ea306b4d6da73ded76527d
Henryvw <Henryvw@gmail.com>
2014-09-21 14:55:12 +0200

    added datetime strftime for rehearsals and started work on putting together index of exercise rehearsals. this still needs some thought re: how this index should be organized in a natural way between creating exercises and rehearsing them, and being able to see all of your rehearsals of a particular exercise, and editing exercises versus editing answers etc.

--- app/models/rehearsal.rb
+++ app/models/rehearsal.rb
@@ -1,7 +1,7 @@
 class Rehearsal < ActiveRecord::Base
 belongs_to :exercise
 has_many :e_questions, through: :exercises
-has_many :e_answers
+has_many :e_answers, :dependent => :destroy
 accepts_nested_attributes_for :e_answers
 accepts_nested_attributes_for :e_questions
 end
