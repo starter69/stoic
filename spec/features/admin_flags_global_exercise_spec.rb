ce98ce3eed60b238798ae9361b1dbf6b7c82d869
Henryvw <Henryvw@gmail.com>
2016-02-25 08:45:16 +0100

    got rspec tests working

--- spec/features/admin_flags_global_exercise_spec.rb
+++ spec/features/admin_flags_global_exercise_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 require 'capybara/rails'
 
-
-
 RSpec.feature 'Admin Flags Global Exercise' do
   scenario 'Exercise is unflagged' do
     sign_in_as_admin
@@ -22,22 +20,24 @@ RSpec.feature 'Admin Flags Global Exercise' do
     fill_in "user_password", with: "saintfrancis"
     click_button "Log in"
   end
+
   def create_exercise
     @exercise = Exercise.new
     @exercise.title = "test title"
+    @exercise.general_description = "test description"
     @exercise.user_id =  @admin_user.id
     @exercise.save!
   end
+
   def flag_it_as_global
     visit edit_exercise_path(@exercise.id)
+    check 'exercise[global]'
+    @exercise.global = true
+    click_button "Save Exercise"
     save_and_open_page
-    binding.pry
-    check 'Publish this exercise globally to all stoic compass users'
   end
 
-
-    # tick global checkbox next to an exercise
-
+  # tick global checkbox next to an exercise
   def check_if_exercise_now_tagged_as_global
     expect(@exercise.global?).to eq true
   end
