89fab02eb5c93f0b8afc258573cf88e3c061d335
Henryvw <Henryvw@gmail.com>
2017-01-18 15:52:58 +0100

    fixed bug with custom exercises causing error in tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,8 +1,11 @@
 class TagsController < ApplicationController
   def index
-    @exercises = Exercise.tagged_with(params[:tag]).where(user_id:current_user.id)
     @global_exercises = Exercise.tagged_with(params[:tag]).where(global:true)
     @quotations = Quotation.all.tagged_with(params[:tag])
     @doctrines = Doctrine.all.tagged_with(params[:tag])
+    #Just needs a bit of work to get the below custom exercises included in the tags. 
+    #if user_signed_in?  
+    #  @exercises = Exercise.tagged_with(params[:tag]).where(user_id:current_user.id)
+    #end
   end
 end
