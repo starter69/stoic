9365904a705a1e098044623073589c3715a21e74
Henryvw <Henryvw@gmail.com>
2017-02-08 12:23:46 +0100

    Added has_many relationship for rehearsals & user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :rehearsals
   has_many :exercises
 
   class MyMailer < Devise::Mailer
