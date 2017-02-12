ed3713baddd39b2b747c4fd1217a8a97ab045e14
Henryvw <Henryvw@gmail.com>
2017-02-12 15:56:06 +0100

    major design changes across virtually all minor pages

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,8 +1,8 @@
 class TagsController < ApplicationController
   def index
     @global_exercises = Exercise.tagged_with(params[:tag]).where(global:true)
-    @quotations = Quotation.all.tagged_with(params[:tag])
-    @doctrines = Doctrine.all.tagged_with(params[:tag])
+    @quotations = Quotation.tagged_with(params[:tag])
+    @doctrines = Doctrine.tagged_with(params[:tag])
     if user_signed_in?  
       @exercises = Exercise.tagged_with(params[:tag]).where(user_id:current_user.id)
     end
