1db3799351bab573a52e492cee4bcc36fa814f47
startjourney9 <jinyixiao69@gmail.com>
2019-05-06 14:59:05 +0200

    remove old commented-out rehearsal and tag tests

--- spec/controllers/exercises_controller_spec.rb
+++ spec/controllers/exercises_controller_spec.rb
@@ -119,8 +119,6 @@ RSpec.describe ExercisesController, type: :controller do
           allow(controller).to receive(:current_user).and_return(current_user)
 
           exercise_params = FactoryBot.attributes_for(:exercise, user_id: current_user.id)
-
-          binding.pry
           post :create, params: { exercise: exercise_params }
           expect { post :create, params: { exercise: exercise_params } }.to change(current_user.exercises, :count).by(1)
         end
@@ -152,72 +150,3 @@ RSpec.describe ExercisesController, type: :controller do
       end
     end
   end
-  #    it 'assigns the return value of exercise#rehearsals_for_user to the rehearsals ivar' do
-  #      exercise = FactoryBot.create(:exercise)
-  #      allow(Exercise).to receive(:find).with(exercise.id.to_s).and_return(exercise)
-  #
-  #      rehearsals_for_user = double('rehearsals_for_user')
-  #      allow(exercise).to receive(:rehearsals_for_user).with(user).and_return(rehearsals_for_user)
-  #
-  #      get :show, id: exercise.id
-  #      rehearsals_ivar = assigns(:rehearsals)
-  #
-  #      expect(rehearsals_ivar).to eq(rehearsals_for_user)
-  #    end
-  #
-  #    context 'when the exercise has no tags' do
-  #      it 'doctrines ivar is nil' do
-  #        exercise = FactoryBot.create(:exercise)
-  #
-  #        get :show, id: exercise.id
-#        doctrines_ivar = assigns(:doctrines)
-#
-#        expect(doctrines_ivar).to be_nil
-#      end
-#    end
-#
-#    context 'when the exercise has tags' do
-#      it 'tags ivar is set to the exercises ivars tags' do
-#        exercise = FactoryBot.create(:exercise)
-#        # Rubocop didn't like all the unused 'exercise_taggings"... apparently I'm not even using them at all for any kind of test
-#        # tag = FactoryBot.create(:tag)
-#        # exercise_tagging = FactoryBot.create(:exercise_tagging, exercise: exercise, tag: tag)
-#        exercise_tags = [tag]
-#
-#        get :show, id: exercise.id
-#        tags_ivar = assigns(:tags)
-#
-#        expect(tags_ivar).to eq(exercise_tags)
-#      end
-#
-#      context 'when the first tag associated with an exercise has no doctrines' do
-#        it 'sets the doctrines ivar to be empty' do
-#          exercise = FactoryBot.create(:exercise)
-#          # tag = FactoryBot.create(:tag)
-#          # exercise_tagging = FactoryBot.create(:exercise_tagging, exercise: exercise, tag: tag)
-#
-#          get :show, id: exercise.id
-#          doctrines_ivar = assigns(:doctrines)
-#
-#          expect(doctrines_ivar).to be_empty
-#        end
-#      end
-#
-#      context 'when the first tag associated with an exercise has doctrines' do
-#        it 'sets the doctrines ivar to the doctrines associated with the first tag' do
-#          exercise = FactoryBot.create(:exercise)
-#          # tag = FactoryBot.create(:tag)
-#          # exercise_tagging = FactoryBot.create(:exercise_tagging, exercise: exercise, tag: tag)
-#          doctrine = FactoryBot.create(:doctrine)
-#          # doctrine_tagging = FactoryBot.create(:doctrine_tagging, tag: tag, doctrine: doctrine)
-#          doctrines_for_first_tag = [doctrine]
-#
-#          get :show, id: exercise.id
-#          doctrines_ivar = assigns(:doctrines)
-#
-#          expect(doctrines_ivar).to eq(doctrines_for_first_tag)
-#        end
-#      end
-#    end
-#   end
-end
