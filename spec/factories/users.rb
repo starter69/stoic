2844f5e1d28993bb085cbfaf5872ef18ad0ad2e7
startjourney9 <jinyixiao69@gmail.com>
2019-05-07 20:35:03 +0200

    add basic exercise model specs

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,7 @@
 FactoryBot.define do
   factory :user do
+    sequence(:email) { |n| "person#{n}@example.com" }
+    sequence(:password) { |n| "amazing_secure_password#{n}" }
+    sequence :id
   end
 end
--- spec/models/exercise_spec.rb
+++ spec/models/exercise_spec.rb
@@ -1,6 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Exercise, type: :model do
+    let(:current_user) { FactoryBot.create(:user) }
+    let(:exercise) { FactoryBot.create(:exercise, user_id: current_user.id) }
+
+  # Basic Setup
+  it 'has a valid factory' do
+    expect(exercise).to be_valid
+  end
+
+  it 'persists a valid model object when given strings for title, general description, user id and a global boolean' do
+    exercise = Exercise.new(
+      title: 'Senecas Dance with Dragons',
+      general_description: 'An amazing exercise to really get your pants off.',
+      user_id: current_user.id
+    )
+  end
+
+  # Classic ActiveRecord Instance Methods
+
+  # Henry's custom methods
   describe '#rehearsals_for_user' do
     it 'successfully fetches all rehearsals for that particular exercise for that particular user' do
       user = FactoryBot.build_stubbed(:user)
