bdbd0834bece3688be85b8ec0a2bac44a621f7c7
startjourney9 <jinyixiao69@gmail.com>
2019-05-10 15:28:03 +0200

    remove manual id-generation for user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,5 @@ FactoryBot.define do
   factory :user do
     sequence(:email) { |n| "person#{n}@example.com" }
     sequence(:password) { |n| "amazing_secure_password#{n}" }
-    sequence :id
   end
 end
