da60f55faf7d218720e1276ab07806ef469bb978
startjourney9 <jinyixiao69@gmail.com>
2019-05-10 11:25:02 +0200

    simplify CanCan exception message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   rescue_from CanCan::AccessDenied do |exception|
-    respond_to do |format|
-      format.json { head :forbidden, content_type: 'text/html' }
-      format.html { redirect_to main_app.root_url, notice: exception.message }
-      format.js   { head :forbidden, content_type: 'text/html' }
-    end
+    redirect_to main_app.root_url, notice: exception.message
   end
 end
