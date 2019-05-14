4c96558338c21806029c5c0fb1a4aebb8320e577
startjourney9 <jinyixiao69@gmail.com>
2019-05-14 11:34:44 +0200

    modernize rehearsal model spec: better factory use

--- spec/factories/rehearsals.rb
+++ spec/factories/rehearsals.rb
@@ -1,4 +1,7 @@
 FactoryBot.define do
   factory :rehearsal do
+    sequence(:exercise_id)
+    sequence(:user_id)
+    sequence(:city) {|n| "city_#{n}" }
   end
 end
--- spec/models/rehearsal_spec.rb
+++ spec/models/rehearsal_spec.rb
@@ -3,49 +3,52 @@ require 'spec_helpers/create_exercise_steps'
 require 'spec_helpers/user_steps'
 
 describe Rehearsal, type: :model do
-  before :each do
-    create_normal_user
-    create_exercise
-    create_rehearsal
+    let(:normal_user) { FactoryBot.create(:user) }
+    let(:rehearsal) { FactoryBot.create(:rehearsal) }
+    let(:exercise) { FactoryBot.create(:exercise) }
+
+  before do
     create_e_question
     create_e_answer
   end
 
   it 'has a valid factory-esque (in this case custom) process for faking rehearsal objects' do
-    expect(@rehearsal).to be_valid
+    expect(rehearsal).to be_valid
   end
 
-  it 'persists a valid model object when given an exercise_id, a user_id and a city.' do
+  it 'creates a valid model object when given an exercise_id, a user_id and a city.' do
     rehearsal = Rehearsal.new(
-      exercise_id: @exercise.id,
-      user_id: @normal_user.id,
+      exercise_id: exercise.id,
+      user_id: normal_user.id,
       city: "Berlin"
     )
     expect(rehearsal).to be_valid
   end
 
   it 'can be read by the user to whom this rehearsal belongs' do
-    expect(Rehearsal.where(user_id = "#{@normal_user.id}")).to exist
+    expect(Rehearsal.where(user_id = "#{normal_user.id}")).to exist
   end
 
-  xit "cannot be read by a user to whom this rehearsal does not belong" do
-    create_normal_user(55)
-    create_second_normal_user(473)
-    create_rehearsal
-    expect(Rehearsal.where(user_id: @second_user.id)).to be_empty
+  it "cannot be read by a user to whom this rehearsal does not belong" do
+    normal_user = FactoryBot.create(:user, id: 55)
+    second_normal_user = FactoryBot.create(:user, id: 473)
+    expect(Rehearsal.where(user_id: second_normal_user.id)).to be_empty
   end
 
   context 'classic Active Record instance methods' do
     it 'returns an exercises id' do
-      expect(@rehearsal.exercise_id).to eq(@exercise.id)
+      exercise = FactoryBot.create(:exercise, rehearsals: [rehearsal])
+      expect(rehearsal.exercise_id).to eq(exercise.id)
     end
 
     it 'returns a users id' do
-      expect(@rehearsal.user_id).to eq(@normal_user.id)
+      normal_user = FactoryBot.create(:user, rehearsals: [rehearsal])
+      expect(rehearsal.user_id).to eq(normal_user.id)
     end
 
     it 'returns a city' do
-      expect(@rehearsal.city).to eq('Berlin')
+      rehearsal = FactoryBot.create(:rehearsal, city: 'Paris')
+      expect(rehearsal.city).to eq('Paris')
     end
   end
 end
