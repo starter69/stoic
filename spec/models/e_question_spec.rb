e5f0663c5818049011a09af04bb427717f53dbfe
startjourney9 <jinyixiao69@gmail.com>
2019-05-15 09:04:04 +0200

    fix simple e_question spec

--- spec/models/e_question_spec.rb
+++ spec/models/e_question_spec.rb
@@ -1,17 +1,21 @@
 require 'rails_helper'
-require 'spec_helpers/create_exercise_steps'
 
 describe EQuestion do
-  before :each do
-    create_exercise
-    create_e_question
-    create_e_answer
-  end
+  let(:e_question) { FactoryBot.create(:e_question) }
 
   it 'returns an exercise question as a string' do
-    expect(@e_question.question). to eq 'Do you practice the Discipline of Assent?'
+    e_question = FactoryBot.create(:e_question, question: 'Do you practice the Discipline of Assent?')
+    expect(e_question.question).to eq 'Do you practice the Discipline of Assent?'
   end
 
-  it 'can be accessed via an e_question'
-  it 'can be accessed via a rehearsal, since it is a nested attribute of rehearsal via e_question'
+  it 'returns a list of its associated e_answers' do
+    e_answer = FactoryBot.create(:e_answer, answer: 'Yes, I love it.')
+    e_question = FactoryBot.create(:e_question, e_answers: [e_answer])
+    expect(e_question.e_answers).to include(e_answer)
+  end
+
+  it 'can be accessed via an exercise' do
+    exercise = FactoryBot.create(:exercise, e_questions: [e_question])
+    expect(exercise.e_questions).to include(e_question)
+  end
 end
