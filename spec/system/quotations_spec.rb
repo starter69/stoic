3a3918a1cc7e11055c6ae7601afbc45aeca4ee9a
startjourney9 <jinyixiao69@gmail.com>
2019-05-23 14:52:10 +0200

    Add #edit

--- spec/system/quotations_spec.rb
+++ spec/system/quotations_spec.rb
@@ -10,8 +10,26 @@ RSpec.describe 'Quotations', type: :system do
 
       expect(page).to have_content 'Goethe Love'
     end
+
+    it 'cannot edit quotations' do
+      quotation = FactoryBot.create(:quotation)
+
+      visit edit_quotation_path(quotation.id)
+
+      expect(page).to have_content 'You are not authorized to access this page'
+    end
+  end
+
+  describe 'Normal Signed-in user' do
+    it 'cannot add or edit quotations' do
+    end
   end
 
   describe 'Admin user creates a new quotation' do
+    it 'can add quotations' do
+    end
+
+    it 'can add quotations' do
+    end
   end
 end
