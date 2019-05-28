90b5b9bd8a6a3a6f3818a9d44d825af315f04eb2
startjourney9 <jinyixiao69@gmail.com>
2019-05-28 12:23:15 +0200

    Add user system specs
    - Add user sign in spec
    - Add user sign out spec
    - Add user sign up spec
    - Add user email already registered spec
    - Add user password confirmation check spec

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,4 +1,61 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :system do
+  let!(:normal_user) { FactoryBot.create(:user) }
+
+  it 'can sign in' do
+    visit root_path
+    click_link 'Sign In'
+    fill_in 'user_email', with: normal_user.email
+    fill_in 'user_password', with: normal_user.password
+    click_button 'Log in'
+
+    expect(page).to have_content 'Signed in successfully.'
+    expect(page).to have_http_status(:success)
+  end
+
+  it 'can sign out' do
+    sign_in normal_user
+    visit root_path
+    click_link 'Sign Out'
+    expect(page).to have_content 'Signed out successfully.'
+    expect(page).to have_http_status(:success)
+  end
+
+  it 'can sign up' do
+    visit root_path
+    click_link 'Sign Up'
+    fill_in 'user_email', with: 'a_new_stoic_is_born_today@gmail.com'
+    fill_in 'user_password', with: 'new_password'
+    fill_in 'user_password_confirmation', with: 'new_password'
+    click_button 'Sign up to activate Stoic Penknife'
+    expect(page).to have_content 'Welcome to Stoic Penknife!'
+    expect(page).to have_http_status(:success)
+  end
+
+  it 'cannot register an email already taken' do
+    visit root_path
+    click_link 'Sign Up'
+
+    expect do
+      fill_in 'user_email', with: normal_user.email
+      fill_in 'user_password', with: normal_user.password
+      fill_in 'user_password_confirmation', with: normal_user.password
+      click_button 'Sign up to activate Stoic Penknife'
+      expect(page).to have_content 'Email has already been taken'
+    end.to change(User, :count).by(0)
+  end
+
+  it 'must match the password in password confirmation' do
+    visit root_path
+    click_link 'Sign Up'
+
+    expect do
+      fill_in 'user_email', with: 'another_new_stoic@gmail.com'
+      fill_in 'user_password', with: 'new_password'
+      fill_in 'user_password_confirmation', with: 'whoops_this_passwords_different'
+      click_button 'Sign up to activate Stoic Penknife'
+      expect(page).to have_content 'Password confirmation doesn\'t match Password'
+    end.to change(User, :count).by(0)
+  end
 end
