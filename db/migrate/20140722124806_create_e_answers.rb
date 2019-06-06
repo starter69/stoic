e8b7fad72cef1323e77e7efffab469b93526a97c
startjourney9 <jinyixiao69@gmail.com>
2019-06-06 14:30:36 +0200

    Fix e_question_id column migration

--- db/migrate/20140722124806_create_e_answers.rb
+++ db/migrate/20140722124806_create_e_answers.rb
@@ -1,7 +1,7 @@
 class CreateEAnswers < ActiveRecord::Migration[5.2]
   def change
     create_table :e_answers do |t|
-      t.integer :question_id
+      t.integer :e_question_id
       t.text :answer
 
       t.timestamps
