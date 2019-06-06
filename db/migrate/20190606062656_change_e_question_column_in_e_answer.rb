43b786aac018a5b0f6bfd28f1d5c8744940eaa86
startjourney9 <jinyixiao69@gmail.com>
2019-06-06 14:24:10 +0200

    Fix e_answers migration

--- db/migrate/20190606062656_change_e_question_column_in_e_answer.rb
+++ db/migrate/20190606062656_change_e_question_column_in_e_answer.rb
@@ -1,5 +1,5 @@
 class ChangeEQuestionColumnInEAnswer < ActiveRecord::Migration[5.2]
   def change
-    rename_column :answers, :e_question_id, :question_id
+    rename_column :e_answers, :e_question_id, :question_id
   end
 end
