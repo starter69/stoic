0d047ef232ae773a656f3c2acb41dc4063f44ad0
startjourney9 <jinyixiao69@gmail.com>
2019-10-23 07:38:54 +0200

    Set up doctrines model with polymorphic tags

--- app/models/doctrine.rb
+++ app/models/doctrine.rb
@@ -1,26 +1,6 @@
 class Doctrine < ActiveRecord::Base
-  has_many :doctrine_taggings
-  has_many :tags, through: :doctrine_taggings
+  include Taggable
 
-  def self.tagged_with(name)
-    Tag.find_by_name!(name).doctrines
-  end
-
-  def self.find_doctrines_with(multiple_tags)
-    doctrines = []
-    multiple_tags.each do |tag|
-      doctrines += Tag.find_by_name!(tag.name).doctrines
-    end
-    doctrines
-  end
-
-  def tag_list
-    tags.map(&:name).join(', ')
-  end
-
-  def tag_list=(names)
-    self.tags = names.split(',').map do |n|
-      Tag.where(name: n.strip).first_or_create!
-    end
-  end
+  has_many :taggings, as: :tagable
+  has_many :tags, through: :taggings
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,9 +2,7 @@ class Tag < ActiveRecord::Base
   has_many :taggings
   has_many :exercises, through: :taggings, source: :tagable, source_type: 'Exercise'
   has_many :quotations, through: :taggings, source: :tagable, source_type: 'Quotation'
-
-  has_many :doctrine_taggings
-  has_many :doctrines, through: :doctrine_taggings
+  has_many :doctrines, through: :taggings, source: :tagable, source_type: 'Doctrine'
 
   validates :name, uniqueness: true
 end
