13d16a26856b013547d66dfeb052bcecdb072b6b
startjourney9 <jinyixiao69@gmail.com>
2019-11-04 14:06:54 +0100

    Write ExerciseTaggings to Taggings migration
    1. Migrate the taggable_ids (associated with exercises) to the new tagable_id.
    2. Enter "Exercise" as every associated row in the tagable_type column. (This is the polymorphic part, so to speak. Now you can just call the Tagging model and it will look up which type is recorded here).
    3. Migrate the ExerciseTagging tag_ids (associated with tags) to the same
    column in Tagging

---
+++ db/migrate/20191104065127_move_exercise_taggings_column_data_to_taggings_table.rb
@@ -0,0 +1,12 @@
+class MoveExerciseTaggingsColumnDataToTaggingsTable < ActiveRecord::Migration[5.2]
+  def change
+    ExerciseTagging.find_each do |et|
+      new_tagging = Tagging.create(
+        tag_id: et.tag_id,
+        tagable_type: "Exercise",
+        tagable_id: et.exercise_id
+      )
+      new_tagging.save!
+    end
+  end
+end
