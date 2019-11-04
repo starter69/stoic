3044e63f735be8932b7887d1743f0cd973115af4
startjourney9 <jinyixiao69@gmail.com>
2019-11-04 14:27:23 +0100

    Write QuotationTaggings to Taggings migration

---
+++ db/migrate/20191104132130_move_quotation_taggings_column_data_to_taggings_table.rb
@@ -0,0 +1,12 @@
+class MoveQuotationTaggingsColumnDataToTaggingsTable < ActiveRecord::Migration[5.2]
+  def change
+    QuotationTagging.find_each do |qt|
+      new_tagging = Tagging.create(
+        tag_id: qt.tag_id,
+        tagable_type: "Quotation",
+        tagable_id: qt.quotation_id
+      )
+      new_tagging.save!
+    end
+  end
+end
