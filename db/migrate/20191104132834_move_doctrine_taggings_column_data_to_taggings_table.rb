e7e90bf8379b5e00adaa8730687aed11827be2d0
startjourney9 <jinyixiao69@gmail.com>
2019-11-04 14:32:06 +0100

    Write DoctrineTaggings to Taggings migration

---
+++ db/migrate/20191104132834_move_doctrine_taggings_column_data_to_taggings_table.rb
@@ -0,0 +1,12 @@
+class MoveDoctrineTaggingsColumnDataToTaggingsTable < ActiveRecord::Migration[5.2]
+  def change
+    DoctrineTagging.find_each do |dt|
+      new_tagging = Tagging.create(
+        tag_id: dt.tag_id,
+        tagable_type: "Doctrine",
+        tagable_id: dt.doctrine_id
+      )
+      new_tagging.save!
+    end
+  end
+end
